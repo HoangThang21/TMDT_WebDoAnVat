0151e3809af2b9a175b2d976e485d09d46b621f7
Thịnh Nhất Lê <68993699+thinhnhatle2002@users.noreply.github.com>
2024-05-17 07:05:37 +0700

    Update settings.js

--- ecommerce-admin/pages/settings.js
+++ ecommerce-admin/pages/settings.js
@@ -1,85 +1,57 @@
 import Layout from "@/components/Layout";
-import { useEffect, useState } from "react";
-import axios from "axios";
 import Spinner from "@/components/Spinner";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { withSwal } from "react-sweetalert2";
 
-function SettingsPage({ swal }) {
+ function SettingsPage({swal}) {
   const [products, setProducts] = useState([]);
-  const [featuredProductId, setFeaturedProductId] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  const [shippingFee, setShippingFee] = useState("");
+  const [featuredProductId, setFeaturedProductId] = useState('');
+  const [productsLoading, setProductsLoading] = useState(false);
+  const [featuredLoading, setFeaturedLoading] = useState(false)
+
 
   useEffect(() => {
-    setIsLoading(true);
-    fetchAll().then(() => {
-      setIsLoading(false);
+    setProductsLoading(true);
+    axios.get("/api/products").then((res) => {
+      setProducts(res.data);
+      setProductsLoading(false);
+    });
+
+    setFeaturedLoading(true);
+    axios.get("/api/settings?name=featuredProductId").then(res => {
+      setFeaturedProductId(res.data.value);
+      setFeaturedLoading(false);
     });
   }, []);
 
-  async function fetchAll() {
-    await axios.get("/api/products").then((res) => {
-      if (res.data) {
-        setProducts(res.data);
-      }
-    });
-    await axios.get("/api/settings?name=featuredProductId").then((res) => {
-      if (res.data) {
-        setFeaturedProductId(res.data.value);
-      }
-    });
-    await axios.get("/api/settings?name=shippingFee").then((res) => {
-      if (res.data) {
-        setShippingFee(res.data.value);
-      }
+  async function saveSettings(){
+    await axios.put('/api/settings',{
+      name: 'featuredProductId',
+      value: featuredProductId,
+    }).then(() => {
+      swal.fire({
+        title: 'Settings saved!',
+        icon:'success',
+      });
     });
   }
-
-  async function saveSettings() {
-    setIsLoading(true);
-    await axios.put("/api/settings", {
-      name: "featuredProductId",
-      value: featuredProductId
-    });
-    await axios.put("/api/settings", {
-      name: "shippingFee",
-      value: shippingFee
-    });
-    setIsLoading(false);
-    swal.fire({
-      title: "Đã lưu cài đặt",
-      icon: "success"
-    });
-  }
-
   return (
     <Layout>
       <h1>Settings</h1>
-      {isLoading && <Spinner />}
-      {!isLoading && (
+      {(productsLoading || featuredLoading) && <Spinner></Spinner>}
+      {(!productsLoading || ! featuredLoading) && (
         <>
           <label>Featured product</label>
-          <select
-            value={featuredProductId}
-            onChange={(ev) => setFeaturedProductId(ev.target.value)}
-          >
-            {products.length > 0 &&
-              products.map((product) => (
-                <option value={product._id} key={product._id}>
-                  {product.title}
-                </option>
-              ))}
+          <select value={featuredProductId} onChange={ev => setFeaturedProductId(ev.target.value)}>
+            {products.length > 0 &&  products.map((product,index) => (
+              <option key={index} value={product._id}>
+                {product.title}
+              </option>
+            ))}
           </select>
-          <label>Shipping price (in usd)</label>
-          <input
-            type="number"
-            value={shippingFee}
-            onChange={(ev) => setShippingFee(ev.target.value)}
-          />
           <div>
-            <button onClick={saveSettings} className="btn-primary">
-              Save settings
-            </button>
+            <button onClick={saveSettings} className="btn-primary">Save settiings</button>
           </div>
         </>
       )}
@@ -87,4 +59,6 @@ function SettingsPage({ swal }) {
   );
 }
 
-export default withSwal(({ swal }) => <SettingsPage swal={swal} />);
+export default withSwal(({swal}) => (
+  <SettingsPage swal={swal} ></SettingsPage>
+));
